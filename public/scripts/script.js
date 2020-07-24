df6baadd2b211d76fe5c3063f3e4324c85a08373
Sedushkin Vladimir <sedushkin1@yandex.ru>
2020-07-24 16:00:49 +0300

    исправлена загрузка выбранной картинки в модальное окно

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', function(){
     let modalOpenLinks = document.querySelectorAll('.image_small');
     let modalCloseLink = document.querySelectorAll('.modal_close');
     let overlay = document.querySelector('#overlay-modal');
-    let modalImgSrc = document.querySelector('.gallery_img');
+    //let modalImgSrc = document.querySelector('.gallery_img');
     let modalImg = document.querySelector('.modalContent');
-    
+
     modalOpenLinks.forEach(function(element) {
         element.addEventListener('click', function(event){
             event.preventDefault();
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function(){
             
             modalEl.classList.add('active');
             overlay.classList.add('active');
-            
+            modalImgSrc = element.firstChild;
             modalImg.src = modalImgSrc.src;
         });
     });
