85b826d1da7b8691870714975c2112f4b2875528
Sedushkin <sedushkin@gmail.com>
2020-07-26 22:51:04 +0300

    Update functions.php

--- engine/functions.php
+++ engine/functions.php
@@ -23,23 +23,27 @@ function renderPage($page_name, $variables = [])
     // если переменных для подстановки не указано, просто
     // возвращаем шаблон как есть
     if (empty($variables)) {
-	      $templateContent = file_get_contents($file);
+          $templateContent = file_get_contents($file);
+          
     } else {
-      	$templateContent = file_get_contents($file);
-      	foreach ($variables as $key => $value) {
-              if ($value != null) {
-    	        // собираем ключи
-              $key = '{{' . strtoupper($key) . '}}';
+      	//$templateContent = file_get_contents($file);
+      	//foreach ($variables as $key => $value) {
+        //      if ($value != null) {
+    	//        // собираем ключи
+        //      $key = '{{' . strtoupper($key) . '}}';
 
           		// заменяем ключи на значения в теле шаблона
-          		$templateContent = str_replace($key, $value, $templateContent);
-	            }
-         }
+//          		$templateContent = str_replace($key, $value, $templateContent);
+//	            }
+        // }
+
+        // заполняем значениями
+        $templateContent = pasteValues($variables, $page_name, $templateContent);
     }
 
     return $templateContent;
 }
-
+/*
 function pasteValues($variables, $page_name, $templateContent){
     foreach ($variables as $key => $value) {
         if ($value != null) {
@@ -70,6 +74,39 @@ function pasteValues($variables, $page_name, $templateContent){
 
     return $templateContent;
 }
+*/
+
+function pasteValues($variables, $page_name, $templateContent){
+    foreach ($variables as $key => $value) {
+        if ($value != null) {
+            // собираем ключи
+            $p_key = '{{' . strtoupper($key) . '}}';
+
+            if(is_array($value)){
+                // замена массивом
+                $result = "";
+                foreach ($value as $value_key => $item){
+                    $itemTemplateContent = file_get_contents(TPL_DIR . "/" . $page_name ."_".$key."_item.tpl");
+
+                    foreach($item as $item_key => $item_value){
+                        $i_key = '{{' . strtoupper($item_key) . '}}';
+
+                        $itemTemplateContent = str_replace($i_key, $item_value, $itemTemplateContent);
+                    }
+
+                    $result .= $itemTemplateContent;
+                }
+            }
+            else
+                $result = $value;
+
+            $templateContent = str_replace($p_key, $result, $templateContent);
+        }
+    }
+
+    return $templateContent;
+}
+
 
 function prepareVariables($page_name){
     $vars = [];
