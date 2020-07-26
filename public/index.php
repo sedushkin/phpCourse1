9fed049b9360d5f6d808067043cf1d7c659b90e1
Sedushkin <sedushkin@gmail.com>
2020-07-26 14:35:14 +0300

    сделаны шаблоны страниц: главная, новостей, отдельной новости, сотрудников, сделан роутер выбора загрузки страниц в index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,19 @@
 <?php
 require_once('../config/config.php');
 
-echo (render(TPL_DIR. '/template_gallery.tpl'));
+$url_array = explode("/",$_SERVER['REQUEST_URI']);
+if ($url_array == "") {
+    $page_name = "index";
+} else {
+    $page_name = $url_array[1];
+}
+$variables = prepareVariables($page_name);
+echo render($page_name,$variables);
 
-gallery_render();
+
+//echo (render(TPL_DIR. '/template_gallery.tpl'));
+
+//gallery_render();
 
 
 
